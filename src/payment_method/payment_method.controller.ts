0424b082f1a7fbb27e3bdf2adf9bb0528c6f359b
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-11-06 08:21:55 +0500

    tugash swagger

--- src/payment_method/payment_method.controller.ts
+++ src/payment_method/payment_method.controller.ts
@@ -3,27 +3,53 @@ import { PaymentMethodService } from './payment_method.service';
 import { CreatePaymentMethodDto } from './dto/create-payment_method.dto';
 import { UpdatePaymentMethodDto } from './dto/update-payment_method.dto';
 import { AdminGuard } from '../common/guards';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('payment-method')
 @Controller('payment-method')
 export class PaymentMethodController {
   constructor(private readonly paymentMethodService: PaymentMethodService) {}
 
+  @ApiOperation({ summary: 'create new payment method' })
+  @ApiResponse({
+    status: 201,
+    description: 'created',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Post('create')
   create(@Body() createPaymentMethodDto: CreatePaymentMethodDto) {
     return this.paymentMethodService.create(createPaymentMethodDto);
   }
 
+  @ApiOperation({ summary: 'get all payment method' })
+  @ApiResponse({
+    status: 201,
+    description: 'getall',
+    type: Object,
+  })
   @Get('get')
   findAll() {
     return this.paymentMethodService.findAll();
   }
 
+  @ApiOperation({ summary: 'get  payment method' })
+  @ApiResponse({
+    status: 201,
+    description: 'get',
+    type: Object,
+  })
   @Get('get/:id')
   findOne(@Param('id') id: string) {
     return this.paymentMethodService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'update  payment method' })
+  @ApiResponse({
+    status: 201,
+    description: 'update',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Patch('update/:id')
   update(
@@ -33,6 +59,12 @@ export class PaymentMethodController {
     return this.paymentMethodService.update(+id, updatePaymentMethodDto);
   }
 
+  @ApiOperation({ summary: 'delete  payment method' })
+  @ApiResponse({
+    status: 201,
+    description: 'delete',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Delete('delete/:id')
   remove(@Param('id') id: string) {
--- src/product_category/product_category.controller.ts
+++ src/product_category/product_category.controller.ts
@@ -3,29 +3,55 @@ import { ProductCategoryService } from './product_category.service';
 import { CreateProductCategoryDto } from './dto/create-product_category.dto';
 import { UpdateProductCategoryDto } from './dto/update-product_category.dto';
 import { AdminGuard } from '../common/guards';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('ProductCategory')
 @Controller('product-category')
 export class ProductCategoryController {
   constructor(
     private readonly productCategoryService: ProductCategoryService,
   ) {}
 
+  @ApiOperation({ summary: 'create new product category' })
+  @ApiResponse({
+    status: 201,
+    description: 'create',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Post('create')
   create(@Body() createProductCategoryDto: CreateProductCategoryDto) {
     return this.productCategoryService.create(createProductCategoryDto);
   }
 
+  @ApiOperation({ summary: 'get all product category' })
+  @ApiResponse({
+    status: 201,
+    description: 'getall',
+    type: Object,
+  })
   @Get('get')
   findAll() {
     return this.productCategoryService.findAll();
   }
 
+  @ApiOperation({ summary: 'get  product category' })
+  @ApiResponse({
+    status: 201,
+    description: 'get',
+    type: Object,
+  })
   @Get('get/:id')
   findOne(@Param('id') id: string) {
     return this.productCategoryService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'update  product category' })
+  @ApiResponse({
+    status: 201,
+    description: 'update',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Patch('update/:id')
   update(
@@ -35,6 +61,12 @@ export class ProductCategoryController {
     return this.productCategoryService.update(+id, updateProductCategoryDto);
   }
 
+  @ApiOperation({ summary: 'delete  product category' })
+  @ApiResponse({
+    status: 201,
+    description: 'delete',
+    type: Object,
+  })
   @UseGuards(AdminGuard)
   @Delete('delete/:id')
   remove(@Param('id') id: string) {
